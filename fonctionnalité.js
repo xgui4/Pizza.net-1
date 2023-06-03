bbe393744646d1c96cede38f34c2e433a058e3ed
xgui4 <xgui2004@gmail.com>
2023-06-02 23:14:31 -0400

    my firts website

---
+++ fonctionnalité.js
@@ -0,0 +1,27 @@
+console.log("J'aime les pizza");
+console.log("Les pizza c'est bon");
+window.alert("Les pizza c'est très bon");
+
+let nom = "Xgui4"; //strings
+console.log("Bonjour," , nom);
+
+let âge; //number
+âge = 18
+console.log("Ton âge est", âge, "ans")
+
+âge = âge + 1 
+
+let élève = true //boolean
+console.log("Étudiant:", élève)
+
+document.getElementById("p1").innerHTML = "Bonjour " + nom;
+document.getElementById("p2").innerHTML = "Tu as " + âge + "ans"
+document.getElementById("p3").innerHTML = "Étudiant " + élève
+
+// commentaire en une ligne 
+
+/* commentaire
+en 
+plusieurs
+lignes
+*/
---
+++ javacript2.js
@@ -0,0 +1,11 @@
+let élève = 20;
+
+//élève = élève + 1
+//élève = élève - 1 
+//élève = élève * 2
+élève = élève / 2
+
+let extraélève = élève % 3;
+
+console.log(élève)
+console.log(extraélève)
